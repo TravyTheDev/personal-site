7c884d4f76fcdf98cadb5cff4d964337a26a5737
TravyTheDev <traviskleckley1@gmail.com>
2024-10-24 02:23:56 +0900

    project-management-page

---
+++ src/projectManagementDataEN.js
@@ -0,0 +1,14 @@
+export const pmData = [
+    {
+        title: "Real time collaboration",
+        image: "/images/live-edit.gif"
+    },
+    {
+        title: "Live notifications",
+        image: "/images/notifications.gif"
+    },
+    {
+        title: "Manage your timeline",
+        image: "/images/timeline.gif"
+    },
+]
